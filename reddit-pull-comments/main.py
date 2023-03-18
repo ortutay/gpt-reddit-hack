b36511cecd8a39f2f79d6903330772395ae7ec0c
ortutay <marcell.ortutay@gmail.com>
2023-03-18 14:45:30 -0700

    Changes

--- reddit-pull-comments/main.py
+++ reddit-pull-comments/main.py
@@ -1,9 +1,12 @@
 import praw
 import os
+import pprint
 
 from praw.models import MoreComments
 from dotenv import load_dotenv
 load_dotenv()
+
+pp = pprint.PrettyPrinter()
 
 print(os.environ.get('CLIENT_SECRET'))
 
@@ -14,22 +17,40 @@ reddit = praw.Reddit(
     user_agent='comment extraction')
 
 
-def get_pairs(url, limit):
-    pairs = []
-    submission = reddit.submission(url=url)
-    q = []
+def get_ids(sub_name, limit):
+    sub = reddit.subreddit(sub_name)
+    ids = []
+    for submission in sub.hot(limit=limit):
+        # print(submission.title, submission.id)
+        ids.append(submission.id)
+    return ids
 
+
+def get_pairs(id, limit):
+    pairs = []
+    submission = reddit.submission(id)
     submission.comments.replace_more(limit=0)
     for top_level in submission.comments:
         for reply in top_level.replies:
+            pairs.append({
+                'comment': top_level.body,
+                'reply': reply.body,
+            })
             print('reply', top_level.body, '==>', reply.body)
 
 
 def main():
     print('main')
 
-    url = "https://www.reddit.com/r/funny/comments/3g1jfi/buttons/"
-    pairs = get_pairs(url, 10)
+    sub_name = 'politics'
+    ids = get_ids(sub_name, 5)
+
+    print(ids)
+
+    for id in ids:
+        pairs = get_pairs(id, 10)
+
+    pp.pprint(pairs)
 
 
 main()
